2adf4d80bda439ffdf4183d06a49be75db59efa1
Pedro Molina <pedro.molina@petroprix.com>
2023-01-12 19:01:25 +0100

    terminado con error

--- app/src/main/java/com/example/loginapirest/MainActivity.kt
+++ app/src/main/java/com/example/loginapirest/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.loginapirest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginapirest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.swType.setOnCheckedChangeListener { button, checked ->
+            button.text = if (checked) getString(R.string.login) else getString(R.string.register)
+            mBinding.btnLogin.text = button.text
+        }
+        mBinding.btnLogin.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        updateUI("(^_^)")
+    }
+
+    private fun updateUI(result: String) {
+        mBinding.tvResult.visibility = View.VISIBLE
+        mBinding.tvResult.text = result
     }
 }
